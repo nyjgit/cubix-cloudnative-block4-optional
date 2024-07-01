a8602c39daf70aab1b55ef077dcead87fc5b4859
nyjgit <162055058+nyjgit@users.noreply.github.com>
2024-07-01 11:48:01 +0200

    Adding Ingress for the frontend

--- raw-kubernetes/scripts/1-setup.ps1
+++ raw-kubernetes/scripts/1-setup.ps1
@@ -4,3 +4,4 @@ kubectl apply -f ..\resources\frontend-deployment.yaml
 kubectl apply -f ..\resources\frontend-service.yaml
 kubectl apply -f ..\resources\backend-deployment.yaml
 kubectl apply -f ..\resources\backend-service.yaml
+kubectl apply -f ..\resources\frontend-ingress.yaml
